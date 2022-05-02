d8b3c002579ad1603459841bf11e4210483078c6
scumbug <6149226+scumbug@users.noreply.github.com>
2022-05-03 03:07:54 +0800

    update wandering merchant logic

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Include dependencies
-const { channel } = require('diagnostics_channel');
 const { Client, Intents, Collection } = require('discord.js');
 const fs = require('fs');
 const http = require('http');
@@ -53,7 +52,7 @@ client.once('ready', async () => {
       console.log('ping');
 
     // set new status
-    client.user.setActivity(`Lost Ark | Valtan: ${status}`);
+    client.user.setActivity(`Lost Ark | ${process.env.LA_SERVER}: ${status}`);
   });
 });
 
@@ -84,51 +83,38 @@ client.on('messageCreate', async (message) => {
 client.login(process.env.TOKEN);
 
 // Wei Card Pinger Function
+/** @param {Message} message */
 const merchantAlerts = async (message) => {
-  const channel = await client.channels.cache.get(process.env.MCT_CHN);
+  const channel = client.channels.cache.get(process.env.ANN_CHN);
 
-  if (!!message.embeds.length) {
-    const s = message.embeds[0].description;
-    const start = '**Item**: ```';
-    const end = '```\n';
-    const item = s.slice(s.indexOf(start) + start.length + 3, s.indexOf(end));
-    const trust = s.includes('Up-and-coming contributor')
-      ? 'Medium'
-      : s.includes('Trusted Voter')
-      ? 'High'
-      : 'Low, check at your risk!';
+  if (message.embeds.length && message.channelId == process.env.MCT_CHN) {
+    const s = message.embeds[0].description.split('\n');
+    const item = s[s.findIndex((line) => line.includes('**Item**')) + 1].slice(
+      0,
+      -3
+    );
+    const location = s.find((line) => line.includes('**Location**'));
 
     // log pings
-    console.log(item.trim() + ' popped');
+    console.log(item + ' popped');
 
     // ping role function
     const pingRoles = async (role) => {
-      console.log(`Alerting peeps with ${item.trim()} role`);
+      console.log(`Alerting peeps with ${item} role`);
       return await channel.send(
-        `${role} **${item}** popped, trust level: **${trust}**`
+        `${role} **${item}** popped \n` +
+          `${location} \n\n` +
+          `More details: ${message.url}`
       );
     };
 
     // ping role for items
+    if (item.includes('Rapport')) pingRoles(process.env.RAPPORT_ROLE);
     if (item.includes('Wei')) pingRoles(process.env.WEI_ROLE);
     else if (item.includes('Seria')) pingRoles(process.env.SERIA_ROLE);
-    else if (item.includes('Rapport')) pingRoles(process.env.RAPPORT_ROLE);
     else if (item.includes('Madnick')) pingRoles(process.env.MADNICK_ROLE);
     else if (item.includes('Sian')) pingRoles(process.env.SIAN_ROLE);
   }
-};
-
-const testPing = async (message) => {
-  const channel = client.channels.cache.get('');
-
-  const merchn = client.channels.cache.get('');
-
-  const msgs = await merchn.messages.fetch({ limit: 100 });
-
-  msgs.forEach((message) => {
-    if (message.embeds[0].description.includes('css'))
-      console.log(message.embeds[0].description);
-  });
 };
 
 const serverStatus = async (name) => {
@@ -172,3 +158,28 @@ const serverStatus = async (name) => {
   // no match found server is down
   return 'Maint';
 };
+
+// Debugging func
+/*
+const testPing = async () => {
+  const channel = client.channels.cache.get('');
+
+  const merchn = client.channels.cache.get('');
+
+  const msgs = await merchn.messages.fetch({ limit: 100 });
+
+  msgs.forEach((message) => {
+    if (message.embeds[0]) {
+      const s = message.embeds[0].description.split('\n');
+      const item = s[
+        s.findIndex((line) => line.includes('**Item**')) + 1
+      ].slice(0, -3);
+      const location = s.find((line) => line.includes('**Location**'));
+
+      channel.send(
+        `roleping\n**${item}** popped. \n${location} \n\nMore Details: ${message.url}`
+      );
+    }
+  });
+};
+*/
